eca4255593dab83617c994028433ce18e4e098af
Hugo Josué lema das neves <131496781+MusgoNato@users.noreply.github.com>
2025-03-13 22:00:36 -0400

    Add Bot Messages

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * Precisa estar habilitado o allow_url_fopen() no .ini
+ */
+
 namespace src\api;
 
 use Dotenv\Dotenv;
@@ -14,28 +18,26 @@ $response = InitSessionController::getSessionToken($_ENV['GLPI_API_INIT_URL'], $
 $session_token = $response->session_token;
 
 // Conecta ao bot no telegram (Executar apenas uma vez, depois comentar)
-$responseTelegram = InitConectionTelegramController::initWebhookTelegram();
+// $responseTelegram = InitConectionTelegramController::initWebhookTelegram();
 
 // Recebe os dados enviados pelo Telegram
 $raw = file_get_contents("php://input");
 
 // No navegador não é tempo real, entao somente por logs consigo ver o que esta acontecendo nas requisições
-$update = json_decode($raw);
-if(!empty($update))
+$InputUserTelegramData = json_decode($raw);
+if(!empty($InputUserTelegramData))
 {
-    error_log("Mensagem separada: " . json_encode($update->message->text));
+    error_log("Mensagem separada: " . json_encode($InputUserTelegramData->message->text));
 }
 
 // Aqui retornaria menu
-if(isset($update->message->text))
+if(isset($InputUserTelegramData->message->text))
 {
-    BotMessagesController::sendMessage($update->message->text);
-}
-else
-{
-    die("erorrrr");
+    BotMessagesController::sendMessage($InputUserTelegramData->message->text, $InputUserTelegramData->message->from->id);
 }
 
- 
+
+// error_log(json_encode($InputUserTelegramData->message->from->id));
+
 
 ?>
--- src/api/BotMessagesController.php
+++ src/api/BotMessagesController.php
@@ -4,11 +4,78 @@ namespace src\api;
 
 class BotMessagesController
 {
-    public static function sendMessage($UserMessage)
+    public static function sendMessage($InputUserMessage, $chatId)
     {
-        if($UserMessage !== 'Menu')
+        // Url da api telegram envio de chat
+        error_log("Mensagem de usuario: " . $InputUserMessage . " Id do chat: " . $chatId);
+
+
+        if($InputUserMessage === "fazer_pedido")
         {
-            var_dump("Peguei o valor : ", $UserMessage);
+            $responseBot = "Ótimo! Vamos fazer seu pedido.\nEscolha um sabor:";
+
+            $keyboard = 
+            [
+                'inline_keyboard' =>
+                [
+                    [
+                        ['text' => 'Calabresa', 'callback_data' => 'sabor_calabresa']
+                    ]
+                ] 
+            ];
+            error_log("Opa deu certo aqui, [1]");
         }
+        else
+        {
+            $responseBot = "Bem vindo ao atendimento do BOT da pizza🍕🤖\nInsira uma das opções abaixo\n[1] - Fazer Pedido\n[2] - Acompanhar pedido\n";
+
+            // Botoes
+            $keyboard = 
+            [
+                'inline_keyboard' =>
+                [ 
+                    [
+                        ['text' => '1 - Fazer pedido', 'callback_data' => 'fazer_pedido'],
+                        ['text' => '2 - Acompanhar pedido', 'callback_data' => 'acompanhar_pedido']
+                    ]
+                ]
+            ];
+        }
+
+        
+        // Prepara dados para requisicao 
+        $data = [
+            'chat_id' => $chatId,
+            'text' => $responseBot,
+        ];
+
+        if($keyboard)
+        {
+            $data['reply_markup'] = json_encode($keyboard, JSON_UNESCAPED_UNICODE);
+        }
+
+        $payload = json_encode($data, JSON_UNESCAPED_UNICODE);
+        
+        // Contexto para o POST
+        $context = stream_context_create([
+            'http' => [
+                'method' => 'POST',
+                'header' => "Content-Type: application/json\r\n" .
+                            "Content-Length: " . strlen($payload) . "\r\n",
+                'content' => $payload
+            ]
+        ]);
+
+        // URL da API para enviar mensagem
+        $url = $_ENV['TELEGRAM_API_URL'] . $_ENV['TELEGRAM_BOT_TOKEN'] . "/sendMessage";
+        $response = file_get_contents($url, false, $context);
+        error_log("Valor response POST : " . $response);
+
+        // logs
+        if($response !== false)
+        {
+            error_log("Resposta do Telegram : " . $response);
+        }
+
     }
 }
